114e7e019e0803156291ed5d4b00e9b672826626
hlccare <280108904@qq.com>
2021-03-28 23:50:56 +0800

    模拟实现didUpdated钩子

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
+import React, { useEffect, useState } from "react";
+import useUpdate from "./useUpdate";
 
-function App() {
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       n: 1
+//     }
+//   }
+//   onClick=()=>{
+//     this.setState(state=>({
+//       n: state.n + 1
+//     }));
+//   }
+//   render(){
+//     return(
+//       <div>
+//         App
+//         <div>
+//           {this.state.n}
+//           <button onClick={this.onClick}>
+//             +1
+//           </button>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+const App = props =>{
+  const childVisible = true
+  const [n,setN] = useState(0)
+  const onClick = ()=>{
+    setN(n+1)
+  }
+
+  useUpdate(()=>{
+      console.log('n变了')
+  },n)
+
   return (
-    <div>hi</div>
-  );
+    <div>
+      {n}
+      <button onClick={onClick}>+1</button>
+    </div>
+  )
+}
+
+const Child = props =>{
+  return (
+    <div>Child</div>
+  )
 }
 
 export default App;
---
+++ src/useUpdate.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+const useUpdate = (fn, dep)=>{
+  const [count,setCount] = useState(0)
+  useEffect(()=>{
+    setCount(x=>x+1)
+  },[dep])
+  useEffect(()=>{
+    if(count>1){
+      fn()
+    }
+  },[count])
+}
+
+export default useUpdate
